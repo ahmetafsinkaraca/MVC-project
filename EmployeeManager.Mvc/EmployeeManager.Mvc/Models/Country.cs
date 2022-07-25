cf32046333559f447972535113b90aed53aeb7fa
ahmetafsinkaraca <103640866+ahmetafsinkaraca@users.noreply.github.com>
2022-07-25 14:54:14 +0300

    Project

---
+++ EmployeeManager.Mvc/EmployeeManager.Mvc/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Mvc.Models
+{
+    [Table("Countries")]
+    public class Country
+    {
+        public int CountryID { get; set; }
+        public string Name { get; set; }
+    }
+
+}
